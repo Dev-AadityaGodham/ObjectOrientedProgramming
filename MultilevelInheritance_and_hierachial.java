5f29f0d0298da6ce7d047fbf55f93e95db86ed8e
Dev-AadityaGodham <159020029+Dev-AadityaGodham@users.noreply.github.com>
2024-03-03 19:43:04 +0530

    Update and rename MultilevelInheritance.java to MultilevelInheritance_and_hierachial.java

--- MultilevelInheritance_and_hierachial.java
+++ MultilevelInheritance_and_hierachial.java
@@ -1,7 +1,7 @@
 /******************************************************************************
     inheritance(one calss able to adapt evey property of other class)
     child class extends the parent class also know as sub-class and base-class
-    ------------------Multilevel inheritance----------------------------
+    --------Multilevel inheritance and Hierchical Inheritance -----------
 *******************************************************************************/
 class shape{
     String shapeName;
@@ -11,11 +11,20 @@ class shape{
         System.out.println("The color of the "+this.shapeName+ " is "+this.color+" and have "+this.sides+ " sides");
     }
 }
+class circle extends shape{
+	
+}
+//class square extends shape calss and similerly Traingle calss and circle also extends shape class is know as
+//Hierchical Inheritance
+class square extends shape{
+
+}
 class Traingle extends shape{
     public void area(int l,int b){
         System.out.println("The area of the "+ this.shapeName +" is"+ 1/2*l*b);
     }
 }
+//multilevle Inheritance:-when derived class extends other drived class which deriver from the base/parent calss known as multilevel Inheritance
 class EquilaterlTraingle extends Traingle{
     
 }
